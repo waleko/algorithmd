27ab926744c617ff5eb2eabeefcc9a02a38c759a
Alex Kovrigin <a.kovrigin0@gmail.com>
2021-05-29 23:57:41 +0300

    halt until auth0 user loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "@auth0/auth0-angular";
+import {Observable} from "rxjs";
+import {User} from "@auth0/auth0-spa-js";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'algorithmd';
 
+  // Flag if user observable has been loaded
+  isDataLoaded: boolean  // TODO: do this cleaner
+
   constructor(public auth: AuthService) {
+    this.isDataLoaded = false
+
+    auth.user$.subscribe(user => {
+      this.isDataLoaded = true
+    })
+  }
+
+  ngOnInit(): void {
+
   }
 }
